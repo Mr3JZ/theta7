a8e452f257ad10a982c06207db05edf01770278f
Alex <blext12@gmail.com>
2017-05-13 15:55:12 +0300

    Project Structure

---
+++ WpfApplication1/IServer.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public interface IServer
+    {
+        void Login(User u, IClient client);
+        void Logout(User u, IClient client);
+        List<Conference> GetConferences();
+        void AddParticipant(Conference c, Participant p);
+        void AddPaper(Conference c, Paper l);
+
+    }
+}
