dab0365dc8e8b57325ee69fb162066245becc854
GALAX\Brie <brieandreiv@yahoo.com>
2017-05-13 15:21:43 +0300

    no message

--- Model/Session.cs
+++ Model/Session.cs
@@ -10,18 +10,18 @@ namespace Model
         public int IdSession { get; set; }
         public List<Reservation> Papers { get; set; }
         public Participant SessionChair { get; set; }
-        public DateTime BegginDate { get; set; }
+        public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
         
 
 
-        public Session(int idSession, List<Reservation> papers, Participant sessionChair, DateTime begginDate, DateTime endDate)
+        public Session(int idSession, List<Reservation> papers, Participant sessionChair, DateTime beginDate, DateTime endDate)
         {
        
             IdSession = idSession;
             Papers = papers;
             SessionChair = sessionChair;
-            BegginDate = begginDate;
+            BeginDate = beginDate;
             EndDate = endDate;
         }
     }
