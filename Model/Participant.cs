a1251d2724978a04dfb8a1b0c985c7a5e3ad1838
GALAX\Brie <brieandreiv@yahoo.com>
2017-05-13 14:56:17 +0300

    no message

--- Model/Participant.cs
+++ Model/Participant.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Participant
+    public class Participant
     {
         //user
         //conference
--- Model/Review.cs
+++ Model/Review.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Review
+     public class Review
     {
         public Participant Reviewer { get; set; }
 
