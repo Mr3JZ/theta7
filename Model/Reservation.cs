07442286bb8803ca58609790280d817fc30af8ba
GALAX\Brie <brieandreiv@yahoo.com>
2017-05-13 14:01:37 +0300

    Model-Reservation,Session

---
+++ Model/Reservation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Reservation
+    {
+        public int IdReservation { get; set; }
+        public Paper Paper { get; set; }
+        public string Room { get; set; }
+        public DateTime BegginHour { get; set; }
+        public int Duration { get; set; }
+        public DateTime Day { get; set; }
+
+        //Constructor with parameters 
+        public Reservation(int idReservation, Paper paper, string room, DateTime begginHour, int duration, DateTime day)
+        {
+            IdReservation = idReservation;
+            Paper = paper;
+            Room = room;
+            BegginHour = begginHour;
+            Duration = duration;
+            Day = day;
+        }
+
+        public Reservation()
+        {
+        }
+
+    }
+}
