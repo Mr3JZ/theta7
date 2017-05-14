9208369279aa831746460b308e5078c7c20871c0
GALAX\Brie <brieandreiv@yahoo.com>
2017-05-14 13:30:39 +0300

    Repo-Add Conference

--- Persistence/Repository/Repo.cs
+++ Persistence/Repository/Repo.cs
@@ -10,6 +10,7 @@ namespace Persistence.Repository
     public class Repo
     {
         List<Payment> payments;
+        List<Conference> conferences;
         public Repo() {
             
         }
@@ -47,6 +48,50 @@ namespace Persistence.Repository
        public List<Payment> getPayments()
         {
             return payments;
+        }
+
+
+        /*Function which adds a new conference.
+         * In:Conference details
+         * Out:new conference in the list
+         * Conditions which are checked in repository:
+         * DeadlineAbstract < DeadlineComplet < DeadlineParticipation < DeadlineBidding < DeadlineEvaluation < BeginDate < EndDate
+         * AdmissionPrice>0
+         * Id-unique
+         */
+        public void addConference(Conference c)
+        {
+            if (c.AdmissionPrice < 1)
+            {
+                throw new RepositoryException("Conference admission price must be >=1!");
+            }
+            if ((DateTime.Compare(c.DeadlineAbstract, c.DeadlineComplet) < 0) && (DateTime.Compare(c.DeadlineComplet, c.DeadlineParticipation) < 0) && (DateTime.Compare(c.DeadlineParticipation, c.DeadlineBidding) < 0))
+            {
+                if ((DateTime.Compare(c.DeadlineBidding, c.DeadlineEvaluation) < 0) && (DateTime.Compare(c.DeadlineEvaluation, c.BeginDate) < 0) && (DateTime.Compare(c.BeginDate, c.EndDate) < 0))
+                {
+                    foreach (Conference conf in getConferences()) {
+                        if (conf.Id == c.Id)
+                        {
+                            throw new RepositoryException("Conference already exist!");
+                        }
+                    }
+
+                    conferences.Add(c);
+                }
+                else
+                {
+                    throw new RepositoryException("Dates must be in chronological order!");
+                }
+            }
+            else
+            {
+                throw new RepositoryException("Dates must be in chronological order!");
+            }
+        }
+
+        public List<Conference> getConferences()
+        {
+            return conferences;
         }
     }
 
---
+++ Persistence/Repository/RepositoryException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    /*Class for throwing exceptions*/
+    public class RepositoryException:Exception
+    {
+        public RepositoryException() { }
+
+        public RepositoryException(string message)
+            : base(message) { }
+        
+        public RepositoryException(string message,Exception inner)
+            :base(message,inner)
+        { }
+    }
+}
