8433190f4af5812047a70d814e818b6d730e815e
Jucatorul <m.radu.cora@gmail.com>
2017-05-20 18:37:10 +0300

    GeneralFrom creat

---
+++ Client/View/GeneralForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+
+namespace Client.View
+{
+    public partial class GeneralForm : Form
+    {
+        public GeneralForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
